a797c3c5bb88e5f51698f5780a751a92fc53f279
arianagh <arian1999@protonmail.com>
2024-08-11 18:05:07 +0330

    feat(global): added check for missing migrations pre commit

--- account/models.py
+++ account/models.py
@@ -6,6 +6,7 @@ from utilities.models.base_model import BaseModel
 
 class User(AbstractUser, BaseModel):
     email = models.EmailField(unique=True, db_index=True, null=True, blank=True)
+    h = models.CharField()
 
     def __str__(self):
         return f"{self.username}-{self.created_at}"
